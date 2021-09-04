b0b6a858801b9615e1cdbf79e6552bf2dc0f1e2f
ducktrshessami <jwee761@gmail.com>
2021-09-04 16:37:11 -0700

    voicechannelconnect promise

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,11 +2,19 @@ import { VoiceConnection } from "@discordjs/voice";
 import { VoiceChannel } from "discord.js";
 
 declare module "discord-voice" {
-    type VoiceChannelConnectOptions = {
+    type VoiceChannelConnectSyncOptions = {
         group?: String,
         selfDeaf?: Boolean,
         selfMute?: Boolean
     };
 
-    export function voiceChannelConnectSync(channel: VoiceChannel, options?: VoiceChannelConnectOptions): VoiceConnection;
+    type VoiceChannelConnectOptions = {
+        group?: String,
+        selfDeaf?: Boolean,
+        selfMute?: Boolean,
+        timeout?: Number
+    };
+
+    export function voiceChannelConnectSync(channel: VoiceChannel, options?: VoiceChannelConnectSyncOptions): VoiceConnection;
+    export function voiceChannelConnect(channel: VoiceChannel, options?: VoiceChannelConnectSyncOptions): Promise<VoiceConnection>;
 }
