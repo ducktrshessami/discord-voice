54cc7d35ab3287d0f68dddd8e98735b122cc464a
ducktrshessami <jwee761@gmail.com>
2021-09-12 20:54:46 -0700

    fixed proto return type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,26 +1,26 @@
 import { VoiceConnection } from "@discordjs/voice";
 import { VoiceChannel } from "discord.js";
 
+type VoiceChannelConnectSyncOptions = {
+    group?: String,
+    selfDeaf?: Boolean,
+    selfMute?: Boolean
+};
+
+type VoiceChannelConnectOptions = {
+    group?: String,
+    selfDeaf?: Boolean,
+    selfMute?: Boolean,
+    timeout?: Number
+};
+
 declare module "discord-voice" {
-    type VoiceChannelConnectSyncOptions = {
-        group?: String,
-        selfDeaf?: Boolean,
-        selfMute?: Boolean
-    };
-
-    type VoiceChannelConnectOptions = {
-        group?: String,
-        selfDeaf?: Boolean,
-        selfMute?: Boolean,
-        timeout?: Number
-    };
-
     export function voiceChannelConnectSync(channel: VoiceChannel, options?: VoiceChannelConnectSyncOptions): VoiceConnection;
     export function voiceChannelConnect(channel: VoiceChannel, options?: VoiceChannelConnectOptions): Promise<VoiceConnection>;
 }
 
 declare module "discord.js" {
     interface VoiceChannel {
-        connect(options?: VoiceChannelConnectOptions): VoiceConnection;
+        connect(options?: VoiceChannelConnectOptions): Promise<VoiceConnection>;
     }
 }
